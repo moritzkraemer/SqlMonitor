e7ffcb8b710ef401266499d4c9dff39e2ec088df
Moritz Krämer <moritz.kraemer28@gmail.com>
2020-12-11 11:13:20 +0100

    .

---
+++ SqlMonitor/DatabaseHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlMonitor
+{
+
+
+    class DatabaseHandler
+    {
+        SqlConnection SqlConnection;
+
+        public DatabaseHandler(string host, string username, string password)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnectionStringBuilder.DataSource = host;
+            sqlConnectionStringBuilder.UserID = username;
+            sqlConnectionStringBuilder.Password = password;
+            SqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public void setDatabase(string database)
+        {
+            SqlCommand command = new SqlCommand("USE " + database, SqlConnection);
+            //command.Parameters.Add("@db", SqlDbType.VarChar);
+            //command.Parameters["@db"].Value = database;
+
+            try
+            {
+                SqlConnection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public DataTable RunQuery(string query)
+        {
+            DataTable result = new DataTable();
+            SqlCommand command = new SqlCommand(query, SqlConnection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            try
+            {
+                sqlDataAdapter.Fill(result);
+            }
+            catch (Exception ex)
+            {
+                sqlDataAdapter.Dispose();
+                throw ex;
+            }
+
+            sqlDataAdapter.Dispose();
+            return result;
+        }
+    }
+}
---
+++ SqlMonitor/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SqlMonitor
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DatabaseHandler DatabaseHandler;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DatabaseHandler = new DatabaseHandler(Host_TextBox.Text, User_TextBox.Text, Password_PasswordBox.Password);
+            DataTable table = DatabaseHandler.RunQuery("SELECT name FROM master.sys.databases");
+            List<string> dbs = table.AsEnumerable().Select(r => r.Field<string>("name")).ToList();
+            TreeView_TreeView.Items.Clear();
+            foreach(string db in dbs)
+            {
+                TreeView_TreeView.Items.Add(db);
+            }
+            
+        }
+
+        private void LogConsole(string s)
+        {
+            textBoxDebug.AppendText(Environment.NewLine + s);
+            textBoxDebug.ScrollToEnd();
+        }
+
+        private void Query_TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key.Equals(Key.Enter))
+            {
+                try
+                {
+                    DataTable table = DatabaseHandler.RunQuery(new TextRange(Query_TextBox.Document.ContentStart, Query_TextBox.Document.ContentEnd).Text);
+                    DataGrid.ItemsSource = table.DefaultView;
+                }
+                catch(Exception ex)
+                {
+                    LogConsole(ex.Message);
+                }
+                
+            }
+        }
+        
+        private void TreeView_TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            LogConsole(TreeView_TreeView.SelectedItem.ToString() + " wurde ausgewählt.");
+            DatabaseHandler.setDatabase(TreeView_TreeView.SelectedItem.ToString());
+        }
+    }
+}
